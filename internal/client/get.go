5205f9c96e58d72fc22b22d60fb46bbdfcccb9ff
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-06-27 10:57:24 +0300

    make Get a func instead of receiver func

--- internal/client/get.go
+++ internal/client/get.go
@@ -5,7 +5,10 @@ import (
 )
 
 // Get sends an HTTP GET request.
-func (c *Client) Get(url string, headers map[string]string) (*http.Response, error) {
+func Get(url string, headers map[string]string) (*http.Response, error) {
+	// Create a new HTTP client
+	httpClient := &http.Client{}
+
 	// Create an HTTP request
 	httpRequest, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
@@ -18,7 +21,7 @@ func (c *Client) Get(url string, headers map[string]string) (*http.Response, err
 	}
 
 	// Send the HTTP request
-	httpResponse, err := c.httpClient.Do(httpRequest)
+	httpResponse, err := httpClient.Do(httpRequest)
 	if err != nil {
 		return nil, err
 	}
