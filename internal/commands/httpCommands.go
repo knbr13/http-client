cb6500b8e7350403f1c9d79fcb3e33e4d46922da
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-06-27 11:13:07 +0300

    move httpCommands into commands/

---
+++ internal/commands/httpCommands.go
@@ -0,0 +1,25 @@
+package internal
+
+import "net/http"
+
+type httpCommand struct {
+	name        string
+	description string
+	run         func(string, []byte, map[string]string) (*http.Response, error)
+}
+
+func getHttpCommands() map[string]httpCommand {
+	return map[string]httpCommand{
+		"httpget": {
+			name:        "httpget",
+			description: "Sends an HTTP GET request",
+			run:         Get,
+		},
+		"httppost": {
+			name:        "httppost",
+			description: "Sends an HTTP POST request",
+			run:         Post,
+		},
+		// Add more commands here...
+	}
+}
