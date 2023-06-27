c06b651405e5d63ea48e62e235706ca6303b2b97
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-06-27 20:10:55 +0300

    implement 'parsePatchInput' func

--- internal/httpMethods/patch.go
+++ internal/httpMethods/patch.go
@@ -2,7 +2,10 @@ package httpmethods
 
 import (
 	"bytes"
+	"fmt"
 	"net/http"
+
+	"github.com/abdullah-alaadine/http-client/internal/utilities"
 )
 
 // Patch sends an HTTP PATCH request.
@@ -28,4 +31,30 @@ func Patch(url string, body []byte, headers map[string]string) (*http.Response, 
 	}
 
 	return httpResponse, nil
+}
+
+// parsePatchInput parses the input slice for the PATCH command and extracts the URL, body, and headers.
+func parsePatchInput(input []string) (string, []byte, map[string]string, error) {
+	if len(input) < 2 || len(input) > 3 {
+		return "", nil, nil, fmt.Errorf("invalid input for PATCH command")
+	}
+
+	url := input[0]
+	bodyStr := input[1]
+	headersStr := ""
+	if len(input) == 3 {
+		headersStr = input[2]
+	}
+
+	body, err := utilities.ParseBody(bodyStr)
+	if err != nil {
+		return "", nil, nil, err
+	}
+
+	headers, err := utilities.ParseHeaders(headersStr)
+	if err != nil {
+		return "", nil, nil, err
+	}
+
+	return url, body, headers, nil
 }
