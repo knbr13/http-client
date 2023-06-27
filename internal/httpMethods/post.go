97e732087b37ae6d7222aee0b2fa1f4b4bd7bb00
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-06-27 19:38:52 +0300

    implement 'parsePostInput' func

--- internal/httpMethods/post.go
+++ internal/httpMethods/post.go
@@ -2,11 +2,14 @@ package httpmethods
 
 import (
 	"bytes"
+	"fmt"
 	"net/http"
+
+	"github.com/abdullah-alaadine/http-client/internal/utilities"
 )
 
 // Post sends an HTTP POST request.
-func Post(url string, body []byte, headers map[string]string) (*http.Response, error) {
+func post(url string, body []byte, headers map[string]string) (*http.Response, error) {
 	// Create a new HTTP client
 	httpClient := &http.Client{}
 
@@ -28,4 +31,26 @@ func Post(url string, body []byte, headers map[string]string) (*http.Response, e
 	}
 
 	return httpResponse, nil
+}
+
+// parsePostInput parses the input slice for the POST command and extracts the URL, body, and headers.
+func parsePostInput(input []string) (string, []byte, map[string]string, error) {
+	if len(input) < 2 || len(input) > 3 {
+		return "", nil, nil, fmt.Errorf("invalid input for POST command")
+	}
+
+	url := input[0]
+	body := []byte(input[1])
+	headersStr := ""
+
+	if len(input) == 3 {
+		headersStr = input[2]
+	}
+
+	headers, err := utilities.ParseHeaders(headersStr)
+	if err != nil {
+		return "", nil, nil, err
+	}
+
+	return url, body, headers, nil
 }
