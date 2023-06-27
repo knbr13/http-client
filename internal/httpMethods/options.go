a356535ad517a60905996af8e433f3b19fe125cc
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-06-27 20:18:55 +0300

    implement 'RunOptions' func

--- internal/httpMethods/options.go
+++ internal/httpMethods/options.go
@@ -1,11 +1,24 @@
 package httpmethods
 
 import (
+	"fmt"
 	"net/http"
+
+	"github.com/abdullah-alaadine/http-client/internal/utilities"
 )
 
+// RunOptions executes the OPTIONS request using the provided input parameters.
+func RunOptions(input []string) (*http.Response, error) {
+	url, headers, err := parseOptionsInput(input)
+	if err != nil {
+		return nil, err
+	}
+
+	return options(url, headers)
+}
+
 // Options sends an HTTP OPTIONS request.
-func Options(url string, body []byte, headers map[string]string) (*http.Response, error) {
+func options(url string, headers map[string]string) (*http.Response, error) {
 	// Create a new HTTP client
 	httpClient := &http.Client{}
 
@@ -27,4 +40,24 @@ func Options(url string, body []byte, headers map[string]string) (*http.Response
 	}
 
 	return httpResponse, nil
+}
+
+// parseOptionsInput parses the input slice for the OPTIONS command and extracts the URL and headers.
+func parseOptionsInput(input []string) (string, map[string]string, error) {
+	if len(input) < 1 || len(input) > 2 {
+		return "", nil, fmt.Errorf("invalid input for OPTIONS command")
+	}
+
+	url := input[0]
+	headersStr := ""
+	if len(input) == 2 {
+		headersStr = input[1]
+	}
+
+	headers, err := utilities.ParseHeaders(headersStr)
+	if err != nil {
+		return "", nil, err
+	}
+
+	return url, headers, nil
 }
