5b3cc5283a036b15e3c2985d148bf407e9735fcc
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-06-27 19:06:25 +0300

    implement 'parseGetInput' func

--- internal/httpMethods/get.go
+++ internal/httpMethods/get.go
@@ -1,7 +1,10 @@
 package httpmethods
 
 import (
+	"fmt"
 	"net/http"
+
+	"github.com/abdullah-alaadine/http-client/internal/utilities"
 )
 
 // Get sends an HTTP GET request.
@@ -25,4 +28,24 @@ func Get(url string, body []byte, headers map[string]string) (*http.Response, er
 	}
 
 	return httpResponse, nil
+}
+
+// parseGetInput parses the input slice for the GET command and extracts the URL and headers.
+func parseGetInput(input []string) (string, map[string]string, error) {
+	if len(input) < 1 || len(input) > 2 {
+		return "", nil, fmt.Errorf("invalid input for GET command")
+	}
+
+	url := input[0]
+	headersStr := ""
+	if len(input) == 2 {
+		headersStr = input[1]
+	}
+
+	headers, err := utilities.ParseHeaders(headersStr)
+	if err != nil {
+		return "", nil, err
+	}
+
+	return url, headers, nil
 }
