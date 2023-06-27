079e0589aceafd05be398bc4a89289c1d5933574
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-06-27 20:20:55 +0300

    implement 'RunHead' func

--- internal/httpMethods/head.go
+++ internal/httpMethods/head.go
@@ -1,11 +1,24 @@
 package httpmethods
 
 import (
+	"fmt"
 	"net/http"
+
+	"github.com/abdullah-alaadine/http-client/internal/utilities"
 )
 
+// RunHead executes the HEAD request using the provided input parameters.
+func RunHead(input []string) (*http.Response, error) {
+	url, headers, err := parseHeadInput(input)
+	if err != nil {
+		return nil, err
+	}
+
+	return head(url, headers)
+}
+
 // Head sends an HTTP HEAD request.
-func Head(url string, body []byte, headers map[string]string) (*http.Response, error) {
+func head(url string, headers map[string]string) (*http.Response, error) {
 	// Create a new HTTP client
 	httpClient := &http.Client{}
 
@@ -27,4 +40,24 @@ func Head(url string, body []byte, headers map[string]string) (*http.Response, e
 	}
 
 	return httpResponse, nil
+}
+
+// parseHeadInput parses the input slice for the HEAD command and extracts the URL and headers.
+func parseHeadInput(input []string) (string, map[string]string, error) {
+	if len(input) < 1 || len(input) > 2 {
+		return "", nil, fmt.Errorf("invalid input for HEAD command")
+	}
+
+	url := input[0]
+	headersStr := ""
+	if len(input) == 2 {
+		headersStr = input[1]
+	}
+
+	headers, err := utilities.ParseHeaders(headersStr)
+	if err != nil {
+		return "", nil, err
+	}
+
+	return url, headers, nil
 }
