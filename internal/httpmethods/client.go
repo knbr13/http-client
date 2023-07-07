ea9dc4125de941f53f6492d7c38901b474ae3492
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-07-07 17:36:20 +0300

    define Input type

--- internal/httpmethods/client.go
+++ internal/httpmethods/client.go
@@ -5,6 +5,13 @@ import (
 	"time"
 )
 
+type Input struct {
+	HTTPMethod string `arg:"-m,--http-method"`
+	URL        string `arg:"-u,--url"`
+	Body       string `arg:"-b,--body"`
+	Header     string `arg:"-H,--header"`
+}
+
 var httpClient *http.Client = &http.Client{
 	Timeout: 30 * time.Second,
 }
