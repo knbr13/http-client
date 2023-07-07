3a59b5ad9cf54069410604fb29e4665988685f83
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-07-07 21:30:10 +0300

    fix headers parsing

--- internal/utilities/input_util.go
+++ internal/utilities/input_util.go
@@ -15,26 +15,13 @@ func ParseHeaders(headersStr string) (map[string]string, error) {
 		return headers, nil
 	}
 
-	if !strings.HasPrefix(headersStr, "headers={") || !strings.HasSuffix(headersStr, "}") {
-		return nil, fmt.Errorf("invalid headers format: %s", headersStr)
+	var headerMap map[string]string
+	err := json.Unmarshal([]byte(headersStr), &headerMap)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse headers: %v", err)
 	}
 
-	headersStr = strings.TrimPrefix(headersStr, "headers={")
-	headersStr = strings.TrimSuffix(headersStr, "}")
-
-	headerPairs := strings.Split(headersStr, ",")
-
-	for _, pair := range headerPairs {
-		parts := strings.Split(pair, ":")
-		if len(parts) != 2 {
-			return nil, fmt.Errorf("invalid header format: %s", pair)
-		}
-		key := strings.TrimSpace(parts[0])
-		value := strings.TrimSpace(parts[1])
-		headers[key] = value
-	}
-
-	return headers, nil
+	return headerMap, nil
 }
 
 // ParseBody parses the input body string into a JSON byte slice.
