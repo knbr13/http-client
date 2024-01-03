2b00dbe0962e4813d9dd92c54e66733d591da066
abdullah-alaadine <abdullahalaadine63@gmail.com>
2024-01-03 03:27:58 +0200

    update main func

--- main.go
+++ main.go
@@ -5,8 +5,9 @@ import (
 	"log"
 	"strings"
 
-	"github.com/knbr13/http-client/internal/httpmethods"
 	"github.com/alexflint/go-arg"
+
+	"github.com/knbr13/http-client/internal/httpmethods"
 )
 
 func main() {
@@ -17,15 +18,14 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer httpResponse.Body.Close()
 
-	// Handle the HTTP response based on the command
 	switch strings.ToLower(input.HTTPMethod) {
 	case "head":
 		// For HEAD request, only print the response status
 		printColoredHeaders(httpResponse.Header)
 	default:
 		// For other requests, print the response body
-		defer httpResponse.Body.Close()
 		body, err := io.ReadAll(httpResponse.Body)
 		if err != nil {
 			log.Fatal(err)
