6a7b574386c3a72a514351e8dc98c24ea9597c65
abdullah-alaadine <abdullahalaadine63@gmail.com>
2024-01-03 03:34:10 +0200

    fix bug in parseBody func

--- internal/utilities/input_util.go
+++ internal/utilities/input_util.go
@@ -32,11 +32,11 @@ func ParseBody(bodyStr string) ([]byte, error) {
 		return []byte{}, nil
 	}
 
-	if !strings.HasPrefix(bodyStr, "body={") || !strings.HasSuffix(bodyStr, "}") {
+	if !strings.HasPrefix(bodyStr, "{") || !strings.HasSuffix(bodyStr, "}") {
 		return nil, fmt.Errorf("invalid body format: %s", bodyStr)
 	}
 
-	bodyStr = strings.TrimPrefix(bodyStr, "body={")
+	bodyStr = strings.TrimPrefix(bodyStr, "{")
 	bodyStr = strings.TrimSuffix(bodyStr, "}")
 
 	keyValuePairs := strings.Split(bodyStr, ",")
